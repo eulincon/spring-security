ab5c92cf5b55ee0a2541eb8f4615b1a5f4d5f696
Lincon <linconsilva08@gmail.com>
2020-11-01 21:18:11 -0300

    2:45:20 LOGOUT

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.demo.security;
 import static com.example.demo.security.ApplicationUserPermition.COURSE_WRITE;
 import static com.example.demo.security.ApplicationUserRole.*;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +18,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -31,7 +34,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable() // Will teach us later
+		http
+				.csrf().disable()
 				.authorizeRequests()
 				.antMatchers("/", "index", "/ccs/*", "/js/*").permitAll()
 				.antMatchers("/api/**").hasRole(STUDENT.name())
@@ -42,7 +46,13 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 				.anyRequest()
 				.authenticated()
 				.and()
-				.httpBasic();
+//				.httpBasic();
+				.formLogin()
+				.loginPage("/login").permitAll()
+				.defaultSuccessUrl("/courses", true)
+				.and()
+					.rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+					.key("somethingverysecured");
 	}
 
 	@Override
